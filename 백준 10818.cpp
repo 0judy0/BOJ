78d2cf7bbb012a9a0371138123a394d4afd010fd
helen0 <whwhdtk1313@naver.com>
2020-06-03 12:25:35 +0900

    3

---
+++ 백준 10818.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#define MAXSIZE 1000000
+using namespace std;
+
+int innum[MAXSIZE];
+
+int main()
+{
+	int count, rmv;
+	int max_num = -1000001;
+	int min_num = 1000001;
+	cin >> count;
+	for (int i = 0; i < count; i++)
+	{
+		cin >> innum[i];
+	}
+	
+	for (int i = 0; i < count; i++)
+	{
+		if (max_num < innum[i])
+		{
+			max_num = innum[i];
+		}
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		if (min_num > innum[i])
+		{
+			min_num = innum[i];
+		}
+	}
+	cout << min_num << " " << max_num << endl;
+	return 0;
+}
