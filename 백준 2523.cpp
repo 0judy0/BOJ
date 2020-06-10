16030402307d061b1ab55562f695937028172b69
helen0 <whwhdtk1313@naver.com>
2020-06-10 19:05:53 +0900

    7

---
+++ 백준 2523.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < num - 1; i++)
+	{
+		for (int j = num - i - 1; j > 0; j--)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
